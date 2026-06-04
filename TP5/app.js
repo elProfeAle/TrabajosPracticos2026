96bbe78aba0ec75f114d2c5b5211bb0b20efa1ab
elProfeAle <118386468+elProfeAle@users.noreply.github.com>
2026-06-04 09:07:34 -0300

    4 de junio

--- TP5/app.js
+++ TP5/app.js
@@ -1,27 +1,7 @@
-//Declarar variable 
-let datos
-//Asignar valor 
-datos= "Suarez Cervellini, Suarez Alvarez y Barrozo hagan silecio. "
-//Mostrar valor en consola 
-console.log("hagan silencio")
-console.log(datos)
-//Mostrar valor en alerta
+//Variables 
+console.log('esta funcionando')
+let numero1 = 5
+let numero2
+let numero
+numero2 = 10 + numero
 
-let numero1=100
-let numero2=25
-let suma= numero1+numero2
-let multiplicacion = numero2 * 5
-let division = numero1 / 4
-
-let nombre = "Lionel"
-let apellido = " Messi"
-let nombre_completo = nombre + apellido + ' Es un jugador de la selección argentina'
-console.log(nombre_completo)
-
-let x = 139
-let y = 75
-let c = 93
-
-let resultado
-resultado = (c*c+c*y+x*y)/2
-console.log(resultado)
