fbb722899f013bc2012fc11e2cc47a8b6bebca8d
elProfeAle <118386468+elProfeAle@users.noreply.github.com>
2026-06-22 21:47:38 -0300

    Solucion TP5

--- TP5/app.js
+++ TP5/app.js
@@ -1,6 +1,51 @@
-//Variables 
-console.log('esta funcionando')
-let numero1 = 5
-let numero2
-let numero
-numero2 = 10 + numero
+//Solución propuesta trabajo práctico numero 5
+
+//Ejericio 1
+let num1 = 15
+let num2 = 20
+
+let suma = num1 + num2
+let resta = num1 - num2
+let multiplicacion = num1 * num2
+let division = num1 / num2
+
+//Ejercicio 2
+alert(suma)
+alert(resta)
+console.log(multiplicacion)
+console.log(division)
+
+//Ejercicio 3
+alert('El resultado de la suma es: '+suma)
+alert('El resultado de la resta es '+ resta)
+console.log('El resultado de la multiplicacion es '+ multiplicacion)
+console.log('El resultado de la division es '+ division)
+
+
+//Ejercicio 4
+let nombreAlumno = 'Mateo'
+let apellidoAlumno = 'Rojas'
+let edadAlumno = 12
+
+console.log('El alumno '+ nombreAlumno + ' tiene '+ edadAlumno + ' años' ) 
+
+//Ejercicio 5
+let precio1 = 1235
+let precio2 = 53600
+let precio3 = 45600
+let total = precio1 + precio2 + precio3
+
+console.log('El total a pagar es: $'+total)
+
+//Ejercicio 6
+let comidaFavorita = 'Sorrentinos'
+let colorFavorito  = 'azul'
+let numeroFavorito = 8
+
+console.log('Mi comida favorita es '+ comidaFavorita +', me gusta el color '+colorFavorito+' y mi número favorito es ' + numeroFavorito)
+
+//Ejercicio 7
+let pais = 'Argentina'
+console.log(pais)
+pais = 'Brasil'
+console.log(pais)
