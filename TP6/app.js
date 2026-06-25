904434f86fed4ce1139d0848e93d662fb7819706
elProfeAle <118386468+elProfeAle@users.noreply.github.com>
2026-06-24 23:01:46 -0300

    tp6 solved

--- TP6/app.js
+++ TP6/app.js
@@ -1,30 +1,18 @@
-let estudiantes = [
-    {
-        nombre: 'Bastian',
-        nota: 100,
-        observacion: 'Excelente examen'
-    },{
-        nombre: 'Luisana',
-        nota: 85,
-        observacion: 'Prestar mas atencion al for'
-    },{
-        nombre: 'Anna',
-        nota: 70,
-        observacion: 'Estudiar mas sobre objetos.'
-    }
-]
+let parrafo = document.querySelector('p')
+let botonTexto = document.querySelector('#botonTexto')
+let botonFondo = document.querySelector('#botonFondo')
+let botonColor = document.querySelector('#botonColor')
+let botonLetra = document.querySelector('#botonLetra')
 
-let nombre = document.querySelector('#nombre')
-let boton = document.querySelector('button')
-let observacion = document.querySelector('#observacion')
-let nota = document.querySelector('#nota')
-
-boton.onclick = function(){
-    for (let index = 0; index < estudiantes.length; index++) {
-        if(estudiantes[index].nombre == nombre.value){
-            
-        }
-        
-    }
-
+botonTexto.onclick = function(){
+    parrafo.textContent = 'Texto nuevo generado por el botón'
+}
+botonFondo.onclick = function(){
+    parrafo.style.backgroundColor = '#034a36ff'
+}
+botonColor.onclick = function(){
+    parrafo.style.color = '#efceceff'
+}
+botonLetra.onclick = function(){
+    parrafo.style.fontFamily = 'Verdana'
 }
