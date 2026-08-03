6998ab930efd9985401162ad846f7f3240c0422d
BrunoBedis <118386468+elProfeAle@users.noreply.github.com>
2026-08-03 14:29:50 -0300

    3/8/26

---
+++ TP7/app.js
@@ -0,0 +1,5 @@
+let comidaFavorita = 'Sorrentinos con salsa de champignon'
+let colorFavorito = 'violeta'
+let nombreFamoso = 'Lionel Messi'
+
+console.log('Hola me llamo '+ nombreFamoso + ' mi color favorito es '+ colorFavorito)
